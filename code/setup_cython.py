cf8a3e2ad5b2ebc80f8fef9beb12d1fd0c7b5221
Greg Watson <g.watson@computer.org>
2017-03-07 22:15:39 -0500

    Create setup_cython.py

---
+++ code/setup_cython.py
@@ -0,0 +1,26 @@
+import os
+import numpy
+import pyximport
+
+if os.name == 'nt':
+    if 'CPATH' in os.environ:
+        os.environ['CPATH'] = os.environ['CPATH'] + numpy.get_include()
+    else:
+        os.environ['CPATH'] = numpy.get_include()
+
+    # XXX: we're assuming that MinGW is installed in C:\MinGW (default)
+    if 'PATH' in os.environ:
+        os.environ['PATH'] = os.environ['PATH'] + ';C:\MinGW\bin'
+    else:
+        os.environ['PATH'] = 'C:\MinGW\bin'
+
+    mingw_setup_args = { 'options': { 'build_ext': { 'compiler': 'mingw32' } } }
+    pyximport.install(setup_args=mingw_setup_args)
+
+elif os.name == 'posix':
+    if 'CFLAGS' in os.environ:
+        os.environ['CFLAGS'] = os.environ['CFLAGS'] + ' -I' + numpy.get_include()
+    else:
+        os.environ['CFLAGS'] = ' -I' + numpy.get_include()
+
+    pyximport.install()
